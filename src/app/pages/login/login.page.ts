60062082c31c953aad1896146b116727ca0a91ca
webtoor <fityanalimunshi@gmail.com>
2019-08-22 13:52:51 +0700

    login validation + create method request login to rest-api

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  SignIn = { "email": "", "password": "" };
+  loginForm : FormGroup;
+  submitted = false;
 
-  constructor(public router : Router, public menuCtrl: MenuController, public toastController: ToastController) { }
+  constructor(private formBuilder: FormBuilder,  
+    public authService: AuthService,
+    public router : Router, 
+    public menuCtrl: MenuController,
+    public toastController: ToastController) { 
+    this.loginForm = this.formBuilder.group({
+      'email' : [null, [Validators.required, Validators.email]],
+      'password' : [null, Validators.required],
+    });
+
+  }
 
   ngOnInit() {
   }
@@ -18,9 +31,38 @@ export class LoginPage implements OnInit {
     this.menuCtrl.enable(false);
   }
 
-  submit(){
-    this.router.navigate(['/home']);
+  onFormSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+        return;
+    }
+    console.log(this.loginForm.value)
+      this.authService.Login( this.loginForm.value, 'login')
+      .subscribe(res => {
+        console.log(res)
+        if(res.access_token) {
+          localStorage.setItem('userAuth', JSON.stringify(res));
+          this.router.navigate(['/home', {replaceUrl: true}]);
+        }else{
+          this.presentToast();
+        }
+      }, (err) => {
+        console.log(err);
+      });
 
+}
+
+  
+  get f() { return this.loginForm.controls; }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Anda memasukkan Email dan Password yang salah. Isi dengan data yang benar dan coba lagi',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
   register(){
