9cbbdffb521504e3e12d474f32ee478424c4945a
webtoor <fityanalimunshi@gmail.com>
2019-08-30 13:05:35 +0700

    create form in home + validator

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatIconModule,
     CommonModule,
     FormsModule,
     IonicModule,
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,22 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  laporanForm : FormGroup;
+  submitted = false;
 
-  constructor(public router : Router, public menuCtrl: MenuController, ) {}
+
+  constructor(private formBuilder: FormBuilder, public router : Router, public menuCtrl: MenuController, ) {
+    this.laporanForm = this.formBuilder.group({
+      'judul' : [null, Validators.required],
+      'sektor' : ['', Validators.required],
+      'namaPerusahaan' : [''],
+      'desaKelurahan' : [''],
+      'kecamatan' : [''],
+      'kotaKabupaten' : [''],
+      'provinsi' : [''],
+      'deskripsiLaporan' : ['', Validators.required],
+    });
+  }
 
 
   ngOnInit() {
@@ -18,4 +33,14 @@ export class HomePage {
   pantauSampah(){
     this.router.navigate(['/pantau-sampah']);
   }
+  get f() { return this.laporanForm.controls; }
+
+  onFormSubmit() {
+    this.submitted = true;
+
+    if (this.laporanForm.invalid) {
+      return;
+   }
+    }
+
 }
