06473c5a894b4e622d8cbbd944fa4e2ac0f14ceb
webtoor <fityanalimunshi@gmail.com>
2019-09-03 12:45:30 +0700

    remove page pantau-all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,11 +13,11 @@ const routes: Routes = [
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
     canActivate : [AuthGuardService]
   },
-  { path: 'pantau-sampah', loadChildren: './pages/pantau-sampah/pantau-sampah.module#PantauSampahPageModule', canActivate : [AuthGuardService] },
+  /* { path: 'pantau-sampah', loadChildren: './pages/pantau-sampah/pantau-sampah.module#PantauSampahPageModule', canActivate : [AuthGuardService] },
   { path: 'pantau-sungai', loadChildren: './pages/pantau-sungai/pantau-sungai.module#PantauSungaiPageModule', canActivate : [AuthGuardService] },
   { path: 'pantau-hutan', loadChildren: './pages/pantau-hutan/pantau-hutan.module#PantauHutanPageModule', canActivate : [AuthGuardService] },
   { path: 'pantau-investasi', loadChildren: './pages/pantau-investasi/pantau-investasi.module#PantauInvestasiPageModule', canActivate : [AuthGuardService] },
-  { path: 'pantau-sampah-plastik', loadChildren: './pages/pantau-sampah-plastik/pantau-sampah-plastik.module#PantauSampahPlastikPageModule', canActivate : [AuthGuardService] },
+  { path: 'pantau-sampah-plastik', loadChildren: './pages/pantau-sampah-plastik/pantau-sampah-plastik.module#PantauSampahPlastikPageModule', canActivate : [AuthGuardService] }, */
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
   { path: 'loader', loadChildren: './pages/loader/loader.module#LoaderPageModule', canActivate : [AuthGuardService] }
